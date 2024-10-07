20596fae61e37dbc19ed41c48e5d790ca21db5ac
ManjunathGoudar404 <manjunathgoudar404@gmail.com>
2024-10-08 01:44:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -49,3 +49,115 @@ contact.addEventListener('click',function(){
 close1.addEventListener('click',function(){
     window.open("https://manjunathgoudar404.github.io/FirstPage/")
 })
+
+
+function textToAudio1() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "This is my Details Page.! You can explore it by hovering over the card elements.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio2() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "This is my Coding Skills Page! You can simply read through the content and hover over the elements.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio3() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "This is my  Certification Page! You can use the button to view other certifications. By clicking 'Show Credential,' you'll be directed to the official certification website.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio4() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "These are my Projects Page! You can explore the code by clicking the 'Code' button, and for a demo of each project, use the 'Demo' button. Scroll to see all the projects.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio5() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "These are my resumes! You can explore them by sliding through the options.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio6() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "The resume was successfully downloaded";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio7() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "This is the Message Box Page! You can fill in all the details, and by clicking the 'Submit' button, you will send a message to my email.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio8() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = " This is my Git repository! You can explore all the code and resources.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio9() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = " This is my Contacts Page! You can simply click the icons to connect with! on different social media platforms.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+function textToAudio10() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = " Thank you for visiting my portfolio!";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
